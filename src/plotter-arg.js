f66d4b4ed0be0d6d378b94446ed60f5f19e630d4
Nelo Pauselli <nelopauselli@gmail.com>
2020-08-29 20:41:19 -0300

    agregando casos y muertes segun fecha de contagio

--- src/plotter-arg.js
+++ src/plotter-arg.js
@@ -20,7 +20,344 @@ function ma(source, period) {
     return sma;
 }
 
-function totalArgentina() {
+function totalArgentinaCases() {
+    let canvas = document.getElementById('ar-chart-cases-time');
+    if (!canvas) return;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    var datasource = [];
+    for (var i = 0; i < totalArgDeathsSource[0].rows.length; i++) {
+        var row = {
+            date: totalArgDeathsSource[0].rows[i].date,
+            deaths: totalArgDeathsSource.reduce(function (a, c) {
+                if (c.rows && c.rows.length)
+                    return a + c.rows[i].cases
+                return a;
+            }, 0)
+        }
+        datasource.push(row);
+    }
+
+    datasource.reverse();
+
+    var averageAR7 = ma(datasource, 7);
+    var datasourceAR7 = averageAR7.map(a =>
+        ({ x: a.date, y: a.deaths })
+    );
+    console.log(datasourceAR7);
+
+    var averageAR14 = ma(datasource, 14);
+    var datasourceAR14 = averageAR14.map(a =>
+        ({ x: a.date, y: a.deaths })
+    );
+
+    var averageAR30 = ma(datasource, 30);
+    var datasourceAR30 = averageAR30.map(a =>
+        ({ x: a.date, y: a.deaths })
+    );
+
+    var total = totalArgDeathsSource.reduce(function (a, c) {
+        return a + c.rows.reduce(function (ra, rc) {
+            return ra + rc.deaths;
+        }, 0);
+    }, 0)
+
+    let data = {
+        labels: datasource.map(c => c.date),
+        datasets: [{
+            data: datasource.map(c => c.deaths),
+            backgroundColor: "#4dc9f622",
+            borderColor: "#4dc9f6AA",
+            pointRadius: 0,
+        }, {
+            label: '7 d\u00EDas',
+            data: datasourceAR7,
+            fill: false,
+            backgroundColor: "#4dc9f622",
+            borderColor: "#4dc9f6",
+            borderWidth: 1,
+            pointRadius: 1
+        }, {
+            label: '14 d\u00EDas',
+            data: datasourceAR14,
+            fill: false,
+            backgroundColor: "#ff000011",
+            borderColor: "#ff0000",
+            borderWidth: 1,
+            pointRadius: 1
+        }, {
+            label: '30 d\u00EDas',
+            data: datasourceAR30,
+            fill: false,
+            backgroundColor: "#ff00ff11",
+            borderColor: "#ff00ff",
+            borderWidth: 1,
+            pointRadius: 1
+        }]
+    };
+
+    var ctx = canvas.getContext('2d');
+    new Chart(ctx, {
+        type: 'line',
+        data: data,
+        options: {
+            responsive: true,
+            legend: {
+                display: false,
+            },
+            title: {
+                display: true,
+                text: 'Casos en Argentina seg\u00FAn fecha de inicio de S\u00EDntomas: ' + total + ' personas'
+            },
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }],
+                xAxes: [{
+                    type: "time",
+                    time: {
+                        tooltipFormat: 'll'
+                    },
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Date'
+                    }
+                }]
+            }
+        }
+    });
+}
+
+function totalArgentinaDeaths() {
+    let canvas = document.getElementById('ar-chart-deaths-time');
+    if (!canvas) return;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    var datasource = [];
+    for (var i = 0; i < totalArgDeathsSource[0].rows.length; i++) {
+        var row = {
+            date: totalArgDeathsSource[0].rows[i].date,
+            deaths: totalArgDeathsSource.reduce(function (a, c) {
+                if (c.rows && c.rows.length)
+                    return a + c.rows[i].deaths
+                return a;
+            }, 0)
+        }
+        datasource.push(row);
+    }
+
+    datasource.reverse();
+
+    var averageAR7 = ma(datasource, 7);
+    var datasourceAR7 = averageAR7.map(a =>
+        ({ x: a.date, y: a.deaths })
+    );
+    console.log(datasourceAR7);
+
+    var averageAR14 = ma(datasource, 14);
+    var datasourceAR14 = averageAR14.map(a =>
+        ({ x: a.date, y: a.deaths })
+    );
+
+    var averageAR30 = ma(datasource, 30);
+    var datasourceAR30 = averageAR30.map(a =>
+        ({ x: a.date, y: a.deaths })
+    );
+
+    var deaths = totalArgDeathsSource.reduce(function (a, c) {
+        return a + c.rows.reduce(function (ra, rc) {
+            return ra + rc.deaths;
+        }, 0);
+    }, 0)
+
+    let data = {
+        labels: datasource.map(c => c.date),
+        datasets: [{
+            data: datasource.map(c => c.deaths),
+            backgroundColor: "#4dc9f622",
+            borderColor: "#4dc9f6AA",
+            pointRadius: 0,
+        }, {
+            label: '7 d\u00EDas',
+            data: datasourceAR7,
+            fill: false,
+            backgroundColor: "#4dc9f622",
+            borderColor: "#4dc9f6",
+            borderWidth: 1,
+            pointRadius: 1
+        }, {
+            label: '14 d\u00EDas',
+            data: datasourceAR14,
+            fill: false,
+            backgroundColor: "#ff000011",
+            borderColor: "#ff0000",
+            borderWidth: 1,
+            pointRadius: 1
+        }, {
+            label: '30 d\u00EDas',
+            data: datasourceAR30,
+            fill: false,
+            backgroundColor: "#ff00ff11",
+            borderColor: "#ff00ff",
+            borderWidth: 1,
+            pointRadius: 1
+        }]
+    };
+
+    var ctx = canvas.getContext('2d');
+    new Chart(ctx, {
+        type: 'line',
+        data: data,
+        options: {
+            responsive: true,
+            legend: {
+                display: false,
+            },
+            title: {
+                display: true,
+                text: 'Fallecidos en Argentina: ' + deaths + ' personas'
+            },
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }],
+                xAxes: [{
+                    type: "time",
+                    time: {
+                        tooltipFormat: 'll'
+                    },
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Date'
+                    }
+                }]
+            }
+        }
+    });
+}
+
+function totalArgentinaFutureDeaths() {
+    let canvas = document.getElementById('ar-chart-future-deaths-time');
+    if (!canvas) return;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    var datasource = [];
+    for (var i = 0; i < totalArgDeathsSource[0].rows.length; i++) {
+        var row = {
+            date: totalArgDeathsSource[0].rows[i].date,
+            deaths: totalArgDeathsSource.reduce(function (a, c) {
+                if (c.rows && c.rows.length)
+                    return a + c.rows[i].futureDeaths
+                return a;
+            }, 0)
+        }
+        datasource.push(row);
+    }
+
+    datasource.reverse();
+
+    var averageAR7 = ma(datasource, 7);
+    var datasourceAR7 = averageAR7.map(a =>
+        ({ x: a.date, y: a.deaths })
+    );
+    console.log(datasourceAR7);
+
+    var averageAR14 = ma(datasource, 14);
+    var datasourceAR14 = averageAR14.map(a =>
+        ({ x: a.date, y: a.deaths })
+    );
+
+    var averageAR30 = ma(datasource, 30);
+    var datasourceAR30 = averageAR30.map(a =>
+        ({ x: a.date, y: a.deaths })
+    );
+
+    var deaths = totalArgDeathsSource.reduce(function (a, c) {
+        return a + c.rows.reduce(function (ra, rc) {
+            return ra + rc.deaths;
+        }, 0);
+    }, 0)
+
+    // var ttl = totalArgDeathsSource.filter(function (r) { return r.ttl }).reduce(function (a, c) {
+    //     return a + c.ttl;
+    // }, 0) / totalArgDeathsSource.filter(function (r) { return r.ttl }).length;
+
+    let data = {
+        labels: datasource.map(c => c.date),
+        datasets: [{
+            data: datasource.map(c => c.deaths),
+            backgroundColor: "#4dc9f622",
+            borderColor: "#4dc9f6AA",
+            pointRadius: 0,
+        }, {
+            label: '7 d\u00EDas',
+            data: datasourceAR7,
+            fill: false,
+            backgroundColor: "#4dc9f622",
+            borderColor: "#4dc9f6",
+            borderWidth: 1,
+            pointRadius: 1
+        }, {
+            label: '14 d\u00EDas',
+            data: datasourceAR14,
+            fill: false,
+            backgroundColor: "#ff000011",
+            borderColor: "#ff0000",
+            borderWidth: 1,
+            pointRadius: 1
+        }, {
+            label: '30 d\u00EDas',
+            data: datasourceAR30,
+            fill: false,
+            backgroundColor: "#ff00ff11",
+            borderColor: "#ff00ff",
+            borderWidth: 1,
+            pointRadius: 1
+        }]
+    };
+
+    var ctx = canvas.getContext('2d');
+    new Chart(ctx, {
+        type: 'line',
+        data: data,
+        options: {
+            responsive: true,
+            legend: {
+                display: false,
+            },
+            title: {
+                display: true,
+                text: 'Fallecidos en Argentina seg\u00FAn Inicio de S\u00EDntomas: ' + deaths + ' personas.' // ttl: ' + ttl + ' d\u00EDas'
+            },
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }],
+                xAxes: [{
+                    type: "time",
+                    time: {
+                        tooltipFormat: 'll'
+                    },
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Date'
+                    }
+                }]
+            }
+        }
+    });
+}
+
+function totalArgentinaByRegion() {
     let canvas = document.getElementById('ar-chart-deaths-bars');
     if (!canvas) return;
     canvas.width = window.innerWidth;
@@ -101,7 +438,7 @@ function last14DaysArgentina() {
             },
             title: {
                 display: true,
-                text: 'Fallecidos cada 100.000 habitantes (ultimos 14 días)'
+                text: 'Fallecidos cada 100.000 habitantes (ultimos 14 d\u00EDas)'
             },
             scales: {
                 yAxes: [{
@@ -150,7 +487,7 @@ function dailyDeathsMediaAverageArgentina(elementId, title, subtitle, color, ds)
             borderWidth: 0,
             pointRadius: 0
         }, {
-            label: '7 días',
+            label: '7 d\u00EDas',
             data: datasourceAR7,
             fill: false,
             backgroundColor: color + "22",
@@ -158,7 +495,7 @@ function dailyDeathsMediaAverageArgentina(elementId, title, subtitle, color, ds)
             borderWidth: 1,
             pointRadius: 1
         }, {
-            label: '14 días',
+            label: '14 d\u00EDas',
             data: datasourceAR14,
             fill: false,
             backgroundColor: "#ff000011",
@@ -166,7 +503,7 @@ function dailyDeathsMediaAverageArgentina(elementId, title, subtitle, color, ds)
             borderWidth: 1,
             pointRadius: 1
         }, {
-            label: '30 días',
+            label: '30 d\u00EDas',
             data: datasourceAR30,
             fill: false,
             backgroundColor: "#ff00ff11",
@@ -212,7 +549,10 @@ function dailyDeathsMediaAverageArgentina(elementId, title, subtitle, color, ds)
 }
 
 export default function plotter() {
-    totalArgentina();
+    totalArgentinaCases()
+    totalArgentinaDeaths();
+    totalArgentinaFutureDeaths();
+    totalArgentinaByRegion();
     last14DaysArgentina();
 
     var datasource = totalArgDeathsSource
@@ -222,7 +562,7 @@ export default function plotter() {
         let region = datasource[i];
         dailyDeathsMediaAverageArgentina(`ar-chart-daily-deaths-${i + 1}`,
             `Fallecidos en ${region.name}: ${region.total}`,
-            `(${Math.round(region.average*100)/100} por cada 100.000 hab)`,
+            `(${Math.round(region.average * 100) / 100} por cada 100.000 hab)`,
             "#4dc9f6",
             region.rows);
     }
--- worker-arg.js
+++ worker-arg.js
@@ -22,27 +22,56 @@ function sameDay(d1, d2) {
         d1.getDate() === d2.getDate();
 }
 
+function daysDiff(d1, d2) {
+    const diffTime = Math.abs(d2 - d1);
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+    return diffDays;
+}
 for (var region of regions) {
     region.color = colors[color++ % colors.length];
     region.rows = [];
     region.deaths = 0;
 }
 
+let max = new Date(2000, 1, 1);
+let min = new Date();
+
+console.log(`[${Date()}] Procesando csv...`);
+
 let dataset = [];
 fs.createReadStream('./temp/covid-arg.csv')
     .pipe(csv())
     .on('data', (data) => {
 
-        if (data.clasificacion_resumen === 'Descartado' || data.fallecido !== 'SI')
+        if (data.clasificacion_resumen === 'Descartado')
             return;
+
+        if (data.fecha_inicio_sintomas) {
+            data.fecha_inicio_sintomas = new Date(data.fecha_inicio_sintomas);
+            if (data.fecha_inicio_sintomas > max) max = data.fecha_inicio_sintomas;
+            if (data.fecha_inicio_sintomas < min) min = data.fecha_inicio_sintomas;
+        }
+
+        if (data.fecha_fallecimiento) {
+            data.fecha_fallecimiento = new Date(data.fecha_fallecimiento);
+            if (data.fecha_fallecimiento > max) max = data.fecha_fallecimiento;
+            if (data.fecha_fallecimiento < min) min = data.fecha_fallecimiento;
+
+            data.ttl = daysDiff(data.fecha_inicio_sintomas, data.fecha_fallecimiento);
+        }
 
         dataset.push(data);
     })
     .on('end', () => {
-        let groupsByRegion = groupBy(dataset, "residencia_provincia_nombre");
+        console.log(`[${Date()}] agrupando resultados...`);
+
+        let groupsByRegion = groupBy(dataset, "carga_provincia_nombre");
 
         let limit14 = new Date();
         limit14.setDate(limit14.getDate() - 14);
+
+        console.log(`max: ${max}`);
+        console.log(`min: ${min}`);
 
         for (let regionName of Object.keys(groupsByRegion)) {
             let region = regions.find(r => r.nombre == regionName);
@@ -51,18 +80,25 @@ fs.createReadStream('./temp/covid-arg.csv')
                 continue;
             }
 
-            let groupRegion = groupsByRegion[regionName];
-            region.deathsTotal = groupRegion.length;
-            region.deathsLast14DaysTotal = groupRegion.filter(r => new Date(r.fecha_fallecimiento) >= limit14).length;
+            console.log(`[${Date()}] calculando resultados para ${regionName}...`);
 
-            let date = new Date();
-            let min = new Date(2020, 01, 01);
+            let groupRegion = groupsByRegion[regionName];
+            let groupRegionDeaths = groupRegion.filter(d => d.fallecido === 'SI');
+            region.deathsTotal = groupRegionDeaths.length;
+            region.ttl = groupRegionDeaths.reduce((a, c) => a + c.ttl, 0) / groupRegionDeaths.length;
+            region.deathsLast14DaysTotal = groupRegionDeaths.reduce((a, c) => a + (c.fecha_fallecimiento >= limit14 ? 1 : 0), 0);
+
+            let date = new Date(max);
             while (date > min) {
-                let daily = groupRegion
-                    .filter(r => sameDay(new Date(r.fecha_fallecimiento), date));
+                let dailyCases = groupRegion.reduce((a, c) => a + (sameDay(new Date(c.fecha_inicio_sintomas), date) ? 1 : 0), 0);
+                let dailyDeaths = groupRegionDeaths.reduce((a, c) => a + (sameDay(new Date(c.fecha_fallecimiento), date) ? 1 : 0), 0);
+                let dailyFutureDeaths = groupRegionDeaths.reduce((a, c) => a + (sameDay(new Date(c.fecha_inicio_sintomas), date) ? 1 : 0), 0);
+
                 region.rows.push({
                     date: new Date(date),
-                    deaths: daily.length
+                    cases: dailyCases,
+                    futureDeaths: dailyFutureDeaths,
+                    deaths: dailyDeaths,
                 });
 
                 date.setDate(date.getDate() - 1);
@@ -84,6 +120,7 @@ fs.createReadStream('./temp/covid-arg.csv')
                         averageLast14Days: r.deathsLast14DaysTotal * 100000 / r.poblacion,
                         total: r.deathsTotal,
                         average: r.deathsTotal * 100000 / r.poblacion,
+                        ttl: r.ttl,
                         rows: r.rows
                     }))
             )
