dbea316e10e0cd48137a88ccf177ee4338d82bdf
Nelo Pauselli <nelopauselli@gmail.com>
2022-01-08 21:39:27 -0300

    actualizando packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 
 const path = require('path');
 
@@ -8,7 +9,7 @@ module.exports = {
     entry: {
         world: './src/index.js',
         arg: './src/argentina.js',
-        caba: './src/caba.js',
+        //caba: './src/caba.js',
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -31,19 +32,24 @@ module.exports = {
             }
         ],
     },
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-    },
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             filename: 'index.html', template: './src/index.html'
         }),
-        new HtmlWebpackPlugin({
-            filename: 'argentina.html', template: './src/argentina.html'
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'caba.html', template: './src/caba.html'
+        // new HtmlWebpackPlugin({
+        //     filename: 'argentina.html', template: './src/argentina.html'
+        // }),
+        // new HtmlWebpackPlugin({
+        //     filename: 'caba.html', template: './src/caba.html'
+        // }),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, "src","data"),
+                    to: path.resolve(__dirname, "dist", "data")
+                }
+            ]
         }),
     ]
 };
